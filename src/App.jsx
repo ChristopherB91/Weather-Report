d01dd564d3b2bfa7aa9c231708eea3449e232e78
Chris <chrisbrowne91@gmail.com>
2022-11-19 06:28:20 -0500

    Weather-Report app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <input placeholder = 'Enter city'style={{}}/>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default App;
