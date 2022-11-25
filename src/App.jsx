ce77376318b3803555f47e9b130008e3e30a641c
Chris <chrisbrowne91@gmail.com>
2022-11-25 04:44:40 -0500

    Had to get rid of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=bronx&appid=7d86334c5233bbd3808f73d75e82c147";
   const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => setData(result));
-  }, []);
+  const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7d86334c5233bbd3808f73d75e82c147`;
 
-  // console.log(data);
-
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  };
   return (
     <div className="app">
       <div className="search">
-        <input placeholder="Enter city" style={{ fontSize: "25px" }} />
-        <button>Search</button>
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          className="city"
+          placeholder="Enter city"
+          type = 'text'
+          style={{ fontSize: "25px" }}
+        />
       </div>
       <div className="results">
         <div className="top" style={{ fontSize: "45px" }}>
-          <div className="location">Bronx</div>
+          <div className="location">{data.name}</div>
           <div className="temp">33°F</div>
         </div>
         <div className="bottom" style={{ fontSize: "25px" }}>
@@ -39,3 +48,12 @@ function App() {
 }
 
 export default App;
+
+
+// useEffect(() => {
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((result) => setData(result));
+//   console.log(data);
+//   console.log(location);
+// }, []);
